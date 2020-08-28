1ca3b769fb9ca782ee97a299fef1a163e05f0cf1
BRIAN BACON <Brian.Bacon@xerox.com>
2020-08-28 15:46:56 -0400

    initial
    initial

---
+++ Assets/Scripts/PlayerAction.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+public class PlayerAction : MonoBehaviour
+{
+    public float speed = 0;
+    private Rigidbody rb;
+    private float movementX;
+    private float movementY;
+    public TextMeshProUGUI countText;
+    private int count = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        SetCountText();
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMove(InputValue move)
+    {
+        Vector2 movementVector = move.Get<Vector2>();
+        this.movementX = movementVector.x;
+        this.movementY = movementVector.y;
+    }
+
+    void FixedUpdate()
+    {
+        rb.AddForce(new Vector3(this.movementX, 0.0f, this.movementY) * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count += 1;
+
+            SetCountText();
+        }
+    }
+}
